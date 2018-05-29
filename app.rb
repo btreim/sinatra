1b1c8492b48c4d6656eea0c9a1eed47852f29fbc
btreim <btreim@gmail.com>
2018-05-29 12:05:17 -0500

    initial commit

---
+++ app.rb
@@ -0,0 +1,46 @@
+require "sinatra"
+require "sinatra/reloader"
+
+
+get "/" do
+  word = params["word"]
+  offset = params["offset"].to_i
+  encoded = caeser_cipher(word, offset)
+  erb :index, layout: :main, :locals => {:encoded => encoded}
+end
+
+def caeser_cipher(someString, shift)
+  if someString == nil
+    return ""
+  end
+  letter = someString.split("")
+  upCase= ("A".."Z").to_a
+  lowCase = ("a".."z").to_a
+  encoded = Array.new
+  letter.each do |x|
+    if upCase.include?(x)
+      index = upCase.index(x)
+      newNum = index.to_i + shift.to_i
+      until newNum <= 26
+        newNum = newNum - 26
+      end
+      newLet = upCase[newNum]
+
+      encoded << newLet
+
+    elsif lowCase.include?(x)
+      index = lowCase.index(x)
+      newNum = index.to_i + shift.to_i
+      until newNum <= 26
+        newNum = newNum - 26
+      end
+      newLet = lowCase[newNum]
+
+      encoded << newLet
+
+    else
+      encoded << x
+    end
+  end
+  return encoded.join("")
+end
